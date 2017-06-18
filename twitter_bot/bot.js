370ca1e30dd9a551267dd22c06f0c483c2a83300
dominicsalinger <dominicsalinger@mac.com>
2017-06-18 08:35:58 -0700

    init commit

---
+++ twitter_bot/bot.js
@@ -0,0 +1,45 @@
+
+
+var Twit = require('twit');
+// all the api keys are in config which will not be under version control
+var config = require('./config');
+var T = new Twit(config);
+
+
+console.log('test');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
