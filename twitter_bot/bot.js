3d09bce8189a187080bf3b5f8101d0b68345ddeb
dominicsalinger <dominicsalinger@mac.com>
2017-07-23 15:34:47 -0700

    adding some code

--- twitter_bot/bot.js
+++ twitter_bot/bot.js
@@ -5,10 +5,31 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
+var params = {
+    q: 'rainbow',
+    count: 5
+}
+var tweet = {
+    status: '#bittertwitterhitter from node.js'
+}
 
-console.log('test');
+function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+    }
+}
+function tweeted(err, data, response) {
+    if (err) {
+        console.log('there was an error');
+    } else {
+        console.log('there was no error');
+    }
+    console.log(data);
+}
 
-
+// T.get('search/tweets', params, gotData);
+T.post('statuses/update', tweet, tweeted);
 
 
 
