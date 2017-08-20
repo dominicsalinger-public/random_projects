614771037e94787bd562c7ba7c99426906193f98
dominicsalinger <dominicsalinger@mac.com>
2017-08-19 23:51:49 -0700

    adding react

--- dominicsalinger/www/index.php
+++ dominicsalinger/www/index.php
@@ -1,1 +1,20 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dominic Pearson</title>
+  <script src="js/react/react.js"></script>
+  <script src="js/react/react-dom.js"></script>
+  <script src="js/browser.min.js"></script>
+
+  </head>
+  <body>
+
+    <div id="example"></div>
+    <script type="text/babel">
+      ReactDOM.render(<h1>Hi, this is a test</h1>, document.getElementById('example'));
+    </script>
+  </body>
+</html>
+https://www.youtube.com/watch?v=OzqR10jG1pg&list=PL6gx4Cwl9DGBuKtLgPR_zWYnrwv-JllpA&index=2#t=7.838196
